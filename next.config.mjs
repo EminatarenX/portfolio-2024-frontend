2eff6ca7f1be977dd30f1b1af69ba087abf979d5
Emiliano Nataren Del Rivero <eminataren2002@gmail.com>
2024-09-27 13:58:51 -0600

    changing things nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // images: {
+    //     domains: ['res.cloudinary.com', "portfolio-eminataren.s3.us-east-1.amazonaws.com"]
+    // }
     images: {
-        domains: ['res.cloudinary.com', "portfolio-eminataren.s3.us-east-1.amazonaws.com"]
-    }
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'portfolio-eminataren.s3.us-east-1.amazonaws.com',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
+
 };
 
 export default nextConfig;
