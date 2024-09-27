2da7ffd5a0419b61b24a9907c89827ce1a10d851
Emiliano Nataren Del Rivero <eminataren2002@gmail.com>
2024-09-27 14:08:41 -0600

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // images: {
-    //     domains: ['res.cloudinary.com', "portfolio-eminataren.s3.us-east-1.amazonaws.com"]
+    //     domains: ["portfolio-eminataren.s3.us-east-1.amazonaws.com"]
     // }
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'portfolio-eminataren.s3.us-east-1.amazonaws.com',
-                port: '',
-                pathname: '**',
-            },
-        ],
+        unoptimized: true,
     },
 
 };
